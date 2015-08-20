a924963b5e0f1a8234f737d10badb7b52c47b1f2
Mike Over <michael.over@avarint.com>
2015-08-20 08:38:00 -0800

    Initial commit. Set up angular app with routing for 2 basic pages and ng-view for the single page application.

---
+++ app.js
@@ -0,0 +1,31 @@
+// MODULE
+var weatherApp = angular.module("weatherApp", ["ngRoute", "ngResource"]);
+
+//ROUTES
+weatherApp.config(function($routeProvider) {
+    
+    $routeProvider 
+    
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    })
+        
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })
+    
+});
+
+weatherApp.controller("homeController", ['$scope', function($scope) {
+    
+}]);
+    
+weatherApp.controller("forecastController", ['$scope', function($scope) {
+    
+}]);
+    
+
+    
+    
