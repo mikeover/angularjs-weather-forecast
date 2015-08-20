68fefcfdbd87c50de11ce65ad2c1f29afbcfb7d6
Mike Over <michael.over@avarint.com>
2015-08-20 14:46:09 -0800

    Add weather API to scope by getting it as a resource. Put basic forecast into HTML, add convertToFarenheit functoin to scope and use it in the forecast page

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var weatherApp = angular.module("weatherApp", ["ngRoute", "ngResource"]);
 //ROUTES
 weatherApp.config(function($routeProvider) {
     
-    $routeProvider 
+    $routeProvider
     
     .when('/', {
         templateUrl: 'pages/home.html',
@@ -33,9 +33,19 @@ weatherApp.controller("homeController", ['$scope', 'cityService', function($scop
     
 }]);
     
-weatherApp.controller("forecastController", ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller("forecastController", ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
     
     $scope.city = cityService.city;
+    $scope.weatherApi = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
+        callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherApi.get({q: $scope.city, cnt: 2});
+    
+    $scope.convertToFarenheit = function(degK) {
+     
+        return Math.round((1.8 * (degK - 273) + 32));
+        
+    }
     
 }]);
     
