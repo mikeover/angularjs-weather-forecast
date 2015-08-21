0dd0300dc453fe5de8c42aaad759074334c89d52
Mike Over <michael.over@avarint.com>
2015-08-21 07:21:06 -0800

    Change the home input to a form so the enter key can be pressed. Need to use ng-submit and implement a submit() function in the controller. Use the location service to set the path to the forecast page.

--- controllers.js
+++ controllers.js
@@ -1,4 +1,19 @@
 // CONTROLLERS
+weatherApp.controller("homeController", ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
+    
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city; 
+    });
+    
+    $scope.submit = function() {
+        // inject $location service to send us to the forecast page
+        $location.path("/forecast");
+    };
+    
+}]);
+
 weatherApp.controller("forecastController", ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
     
     $scope.city = cityService.city;
@@ -9,6 +24,7 @@ weatherApp.controller("forecastController", ['$scope', '$resource', '$routeParam
         callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
     
     $scope.weatherResult = $scope.weatherApi.get({q: $scope.city, cnt: $scope.days});
+    //console.log($scope.weatherResult);
     
     $scope.convertToFarenheit = function(degK) {
         return Math.round((1.8 * (degK - 273) + 32));
--- services.js
+++ services.js
@@ -2,13 +2,3 @@
 weatherApp.service('cityService', function() {
     this.city = "Buffalo, NY";
 });
-
-weatherApp.controller("homeController", ['$scope', 'cityService', function($scope, cityService) {
-    
-    $scope.city = cityService.city;
-    
-    $scope.$watch('city', function() {
-        cityService.city = $scope.city; 
-    });
-    
-}]);
